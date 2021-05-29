8d188b3bc5bb7556538dc3e068bb348c88d9c278
Alex <kuanalex96@gmail.com>
2021-05-29 10:01:43 -0400

    updating project.js with getTask addTask deleteTask getTasksToday getTasksThisWeek funtionality

--- src/modules/Project.js
+++ src/modules/Project.js
@@ -27,4 +27,29 @@ export default class Project {
     contains(taskName) {
         return this.tasks.some((task) => task.getName() === taskName);
     }
+
+    addTask(task) {
+        if (this.tasks.indexOf(task) > 0) return;
+        this.tasks.push(task);
+    }
+
+    deleteTask(taskName) {
+        const taskToDelete = this.tasks.find((task) => task.getName() === taskName);
+        this.tasks.splice(this.tasks.indexOf(taskToDelete), 1);
+    }
+
+    getTasksToday() {
+        return this.tasks.filter((task) => {
+            const taskDate = new Date(task.getDateFormatted());
+            return isToday(toDate(taskDate));
+        });
+    }
+
+    getTasksThisWeek() {
+        return this.tasks.filter((task) => {
+            const taskDate = new Date(task.getDateFormatted());
+            return isThisWeek(subDays(toDate(taskDate), 1));
+        });
+    }
+
 }
