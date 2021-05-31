a327e88bfc8395465332e9cb5a3bf184c03d5d25
Alex <kuanalex96@gmail.com>
2021-05-31 12:27:02 -0400

    updating Storage class, setting up todoList

--- src/modules/Storage.js
+++ src/modules/Storage.js
@@ -6,4 +6,30 @@ export default class Storage {
     static saveTodoList(data) {
         localStorage.setItem('todoList', JSON.stringify(data));
     }
+
+    static getTodoList() {
+        // local storage doesn't store type of data so we have to convert it
+
+        const todoList = Object.assign(
+            new TodoList(),
+            JSON.parse(localStorage.getItem('todoList')),
+        );
+
+        todoList.setProjects(
+            todoList
+            .getProjects()
+            .map((project) => Object.assign(new Project(), project)),
+        );
+
+        todoList
+            .getProjects()
+            .forEach((project) =>
+                project.setTasks(
+                    project.getTasks().map((task) => Object.assign(new Task(), task)),
+                ),
+            );
+
+        return todoList;
+    }
+
 }
