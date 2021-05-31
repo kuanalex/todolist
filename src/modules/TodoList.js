2a3d782c9e8b4ce37b629103444c1062474e8e02
Alex <kuanalex96@gmail.com>
2021-05-31 08:14:24 -0400

    adding setProjects() getProjects() and getProject()

--- src/modules/TodoList.js
+++ src/modules/TodoList.js
@@ -1,12 +1,28 @@
-import { compareAsc, toDate } from 'date-fns';
+import {
+    compareAsc,
+    toDate
+} from 'date-fns';
 import Project from './Project';
 import Task from './Task';
 
 export default class TodoList {
     constructor() {
-      this.projects = [];
-      this.projects.push(new Project('Inbox'));
-      this.projects.push(new Project('Today'));
-      this.projects.push(new Project('This week'));
+        this.projects = [];
+        this.projects.push(new Project('Inbox'));
+        this.projects.push(new Project('Today'));
+        this.projects.push(new Project('This week'));
     }
+
+    setProjects(projects) {
+        this.projects = projects;
+    }
+
+    getProjects() {
+        return this.projects;
+    }
+
+    getProject(projectName) {
+        return this.projects.find((project) => project.getName() === projectName);
+    }
+
 }
